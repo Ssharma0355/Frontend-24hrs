61c8515351886f768e050d525be1bba5ffb7c981
Ssharma0355 <ssharma0355@gmail.com>
2026-08-02 13:29:52 +0530

    move -ve and +ve elements in array

--- JavaScriptRevision/DSA/Arrays/MovePositiveElements.js
+++ JavaScriptRevision/DSA/Arrays/MovePositiveElements.js
@@ -1,11 +1,11 @@
-let arr = [0,1,0,0,1,0,1,1,0];
+let arr = [1,-2,-4,5,-6,7,-8,10];
+// make [-2, -4, -6, -8, 1,5,7,10];
 
-// swap all 0 to left and 1 to right
 let i = 0;
 let j = 0;
 
-while(i<arr.length){
-    if(arr[i] === 0){
+while(i < arr.length){
+    if(arr[i] <= 0){
         let temp = arr[i];
         arr[i] = arr[j];
         arr[j] = temp;
