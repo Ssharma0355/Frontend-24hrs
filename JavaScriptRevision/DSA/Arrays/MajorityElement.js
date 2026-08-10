bc5265e71dc1a1e7c5a693bfa69a73429a89685e
Ssharma0355 <ssharma0355@gmail.com>
2026-08-10 15:01:33 +0530

    fixed

--- JavaScriptRevision/DSA/Arrays/MajorityElement.js
+++ JavaScriptRevision/DSA/Arrays/MajorityElement.js
@@ -9,23 +9,15 @@ let half = Math.floor(arr.length/2)
 let store = arr[0]
 let count = 1;
 for(let i = 1 ;i<arr.length;i++){
-    // we check the count and store is equal to current i
-    if( count !== 0 && store === arr[i]){
-        // then we increase the count
+    if(count === 0){
+        store = arr[i];
+        count =1;
+    }
+    else if(store === arr[i]){
         count++;
     }
-    else  if(count !== 0 && store !== arr[i]){
-        // we check the count and store not equal to current i
+    else{
         count--;
-        // we count -1;
     }
-    else if( count === 0 && store !== arr[i]){
-        // we check count is 0 and store is not equal to current i
-        store = arr[i];
-        // we replace store with i
-        count = 1;
-        // and increase the count
-    }
-   
 }
 console.log(store)
